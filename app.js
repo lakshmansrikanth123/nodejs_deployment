b9d8f28d07499fdd5b16b5ce5ea3213fc7c32420
lakshmansrikanth123 <lakshmansrikanthkorla.225@gmail.com>
2026-02-26 12:17:36 +0530

    Fix formatting of bodyParser import

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const bcrypt = require("bcryptjs");
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); 
 
 const app = express();
 
